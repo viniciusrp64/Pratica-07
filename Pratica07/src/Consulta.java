f7b3a60e79ab54939e736685781a81bde824e4ad
viniciusrp64 <62041895+viniciusrp64@users.noreply.github.com>
2023-11-16 21:23:10 -0300

    Add files via upload

---
+++ Pratica07/src/Consulta.java
@@ -0,0 +1,79 @@
+import java.sql.*;
+
+public class Consulta {
+
+    private final String url = "jdbc:postgresql://localhost/DBlivrariaUniversitaria";
+    private final String user = "postgres";
+    private final String password = "123456";
+
+    private static final String QUERY_TITULO = "select * from livro where nm_titulo like ?";
+    private static final String QUERY_VALOR = "select * from livro where vl_preco>=?";
+
+    public void BuscaTitulo(String titulo) throws SQLException {
+        System.out.println(QUERY_TITULO);
+
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(QUERY_TITULO);) {
+            titulo=titulo + "%";
+            preparedStatement.setString(1, titulo);
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                String id = rs.getString("id_isbn");
+                String name_titulo = rs.getString("nm_titulo");
+                System.out.println(id + " - " + name_titulo);
+            }
+        } catch (SQLException e) {
+
+            // print SQL exception information
+            printSQLException(e);
+        }
+    }
+
+
+    public void BuscaValor(Double valor) throws SQLException {
+        System.out.println(QUERY_VALOR);
+
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(QUERY_VALOR);) {
+            preparedStatement.setDouble(1, valor);
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                String id = rs.getString("id_isbn");
+                String name_titulo = rs.getString("nm_titulo");
+                Double valor_livro = rs.getDouble("vl_preco");
+                System.out.println(id + " - " + name_titulo + " - " + valor_livro);
+            }
+        } catch (SQLException e) {
+
+            // print SQL exception information
+            printSQLException(e);
+        }
+    }
+    public static void printSQLException(SQLException ex) {
+        for (Throwable e: ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
